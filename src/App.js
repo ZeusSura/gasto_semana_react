21a2ee810c8417a5e2d6ec1aedf09c4e014a7a0a
ZeusSura <Alejandrogzh@gmail.com>
2020-09-15 18:00:55 -0500

    Agregar props y state de presupuesto y restante

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Pregunta from "./Components/Pregunta";
 import "./App.css";
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [restate, setRestante] = useState(0);
+
   return (
-    <div className="containter">
+    <div className="container">
       <header>
         <h1>Gasto semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Pregunta 
+          setPresupuesto={setPresupuesto} 
+          setRestante={setRestante}
+           />
+        </div>
       </header>
     </div>
   );
